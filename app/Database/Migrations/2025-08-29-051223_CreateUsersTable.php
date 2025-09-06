9bdb4a5184501f99b5bfe8b6bf6949f60f3fc721
Gener154 <generv03@gmail.com>
2025-09-06 08:19:52 +0800

    Added database migrations and Seeders

--- app/Database/Migrations/2025-08-29-051223_CreateUsersTable.php
+++ app/Database/Migrations/2025-08-29-051223_CreateUsersTable.php
@@ -4,6 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
+// Migration for creating users table
 class CreateUsersTable extends Migration
 {
     public function up()
